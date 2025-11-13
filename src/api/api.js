45e0a3322b7128d38bdaa6c578ecc954ac975daf
muhammadsobuj20 <muhammadsobuj20@gmail.com>
2025-11-13 18:21:57 +0600

    Uodated api

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL:
+   "https://import-export-hub-eta.vercel.app",
 });
 
 export default api;
